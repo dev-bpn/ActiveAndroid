903b7fda88846756affb3c882c1217fa798342a8
dev-bpn <jazzbpn@gmail.com>
2015-11-17 13:17:45 +0545

    MyLog class

---
+++ app/src/main/java/comstoresearchqmobotech/google/httpsplay/activeandroid/log/MyLog.java
@@ -0,0 +1,16 @@
+package comstoresearchqmobotech.google.httpsplay.activeandroid.log;
+
+import android.util.Log;
+
+/**
+ * Created by Dell on 11/17/2015.
+ */
+public class MyLog {
+
+    public static final String TAG = "MY_LOG";
+
+    public static void showLog(String message)
+    {
+        Log.i(TAG , message);
+    }
+}
