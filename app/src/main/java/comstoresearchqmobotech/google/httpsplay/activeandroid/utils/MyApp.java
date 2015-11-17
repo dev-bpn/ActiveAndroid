5533d192497a919b7ed803b58a65187036859a11
dev-bpn <jazzbpn@gmail.com>
2015-11-17 13:23:08 +0545

    initialize ActiveAndroid on Application Class Dynamically

--- app/src/main/java/comstoresearchqmobotech/google/httpsplay/activeandroid/utils/MyApp.java
+++ app/src/main/java/comstoresearchqmobotech/google/httpsplay/activeandroid/utils/MyApp.java
@@ -3,6 +3,9 @@ package comstoresearchqmobotech.google.httpsplay.activeandroid.utils;
 import android.app.Application;
 
 import com.activeandroid.ActiveAndroid;
+import com.activeandroid.Configuration;
+
+import comstoresearchqmobotech.google.httpsplay.activeandroid.log.MyLog;
 
 /**
  * Created by Dell on 11/17/2015.
@@ -12,6 +15,11 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        ActiveAndroid.initialize(this);
+//        ActiveAndroid.initialize(this);
+
+        Configuration configuration = new Configuration.Builder(this).setDatabaseName("my_database.db").create();
+        ActiveAndroid.initialize(configuration);
+
+        MyLog.showLog("Active android initialize");
     }
 }
