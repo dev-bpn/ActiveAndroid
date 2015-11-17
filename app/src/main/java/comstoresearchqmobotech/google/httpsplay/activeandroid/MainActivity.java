c41d9c50ca9ac42b519fa1e2615884d51f58a5dc
dev-bpn <jazzbpn@gmail.com>
2015-11-17 15:26:20 +0545

    Inserting and updating records

--- app/src/main/java/comstoresearchqmobotech/google/httpsplay/activeandroid/MainActivity.java
+++ app/src/main/java/comstoresearchqmobotech/google/httpsplay/activeandroid/MainActivity.java
@@ -5,12 +5,34 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import comstoresearchqmobotech.google.httpsplay.activeandroid.database.Category;
+import comstoresearchqmobotech.google.httpsplay.activeandroid.database.Item;
+import comstoresearchqmobotech.google.httpsplay.activeandroid.log.MyLog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Category restaurant = new Category();
+        restaurant.name = "CategoryName1";
+        restaurant.save();
+        MyLog.showLog("category name saved");
+
+        Item item = new Item();
+        item.category = restaurant;
+        item.name = "restaurantName 1";
+        item.save();
+
+        item = new Item();
+        item.category = restaurant;
+        item.name = "Olive Garden";
+        item.save();
+        MyLog.showLog("Restaurants name saved");
+
+
     }
 
     @Override
