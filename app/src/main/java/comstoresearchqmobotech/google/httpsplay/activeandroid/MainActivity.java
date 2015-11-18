38dfce9bef21796cfa22f0e66047b3df12a7ec05
dev-bpn <jazzbpn@gmail.com>
2015-11-18 17:33:54 +0545

    ExpandableLstViewAdapter DONE

--- app/src/main/java/comstoresearchqmobotech/google/httpsplay/activeandroid/MainActivity.java
+++ app/src/main/java/comstoresearchqmobotech/google/httpsplay/activeandroid/MainActivity.java
@@ -4,7 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import comstoresearchqmobotech.google.httpsplay.activeandroid.adapter.ExpandableListAdapter;
 import comstoresearchqmobotech.google.httpsplay.activeandroid.parsing.Apis;
 import comstoresearchqmobotech.google.httpsplay.activeandroid.parsing.MyParser;
 import comstoresearchqmobotech.google.httpsplay.activeandroid.toast.MyToast;
@@ -12,12 +18,50 @@ import comstoresearchqmobotech.google.httpsplay.activeandroid.utils.MyUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ExpandableListView expandableListView;
+    private ExpandableListAdapter expandableListAdapter;
+    private List<String> listDataHeader;
+    private HashMap<String , List<String>> listDataChild;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getData();
+        expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+        prepareDataList();
+        expandableListView.setAdapter(new ExpandableListAdapter(this , listDataHeader , listDataChild));
 
+
+    }
+
+    private void prepareDataList() {
+
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String , List<String>>();
+
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Tomorrow Show");
+        listDataHeader.add("Weekly Show");
+
+        List<String> nowShow = new ArrayList<>();
+        nowShow.add("The Shawshank Redemption");
+        nowShow.add("The Godfather");
+        nowShow.add("The Godfather: Part II");
+
+        List<String> tomorrowShow = new ArrayList<>();
+        tomorrowShow.add("The Conjuring");
+        tomorrowShow.add("Despicable Me 2");
+        tomorrowShow.add("Turbo");
+
+        List<String> weeklyShow = new ArrayList<>();
+        weeklyShow.add("2 Guns");
+        weeklyShow.add("The Smurfs 2");
+        weeklyShow.add("The Spectacular Now");
+
+        listDataChild.put(listDataHeader.get(0) , nowShow);
+        listDataChild.put(listDataHeader.get(1) , tomorrowShow);
+        listDataChild.put(listDataHeader.get(2) , weeklyShow);
 
     }
 
--- app/src/main/java/comstoresearchqmobotech/google/httpsplay/activeandroid/adapter/ExpandableListAdapter.java
+++ app/src/main/java/comstoresearchqmobotech/google/httpsplay/activeandroid/adapter/ExpandableListAdapter.java
@@ -1,9 +1,97 @@
 package comstoresearchqmobotech.google.httpsplay.activeandroid.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+import comstoresearchqmobotech.google.httpsplay.activeandroid.R;
+
 /**
  * Created by Dell on 11/18/2015.
  */
-public class ExpandableListAdapter  {
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
+    private Context context;
+    private List<String> listDataHeader;
+    private HashMap<String , List<String>> listDataChild;
 
+    public ExpandableListAdapter(Context context , List<String> listDataHeader , HashMap<String , List<String>> listDataChild){
+
+        this.context = context;
+        this.listDataHeader = listDataHeader;
+        this.listDataChild = listDataChild;
+
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listDataHeader.size();
+    }
+
+    @Override
+    public int getChildrenCount(int position) {
+        return listDataChild.get(listDataHeader.get(position)).size();
+    }
+
+    @Override
+    public Object getGroup(int position) {
+        return listDataHeader.get(position);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return listDataChild.get(listDataHeader.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+
+        String headerTitle = (String) getGroup(groupPosition);
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_group , null);
+        }
+        TextView textView = (TextView) convertView.findViewById(R.id.myListHeader);
+        textView.setText(headerTitle);
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        String childText = (String) getChild(groupPosition , childPosition);
+        if(convertView == null){
+
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item , null);
+        }
+        TextView textView = (TextView) convertView.findViewById(R.id.myListItem);
+        textView.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
 }
