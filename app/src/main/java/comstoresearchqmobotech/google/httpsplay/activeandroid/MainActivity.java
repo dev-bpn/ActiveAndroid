e61d5a0de8fa3e351dcb6e562fad909d0ca2b1da
dev-bpn <jazzbpn@gmail.com>
2015-11-19 17:34:31 +0545

    makeup

--- app/src/main/java/comstoresearchqmobotech/google/httpsplay/activeandroid/MainActivity.java
+++ app/src/main/java/comstoresearchqmobotech/google/httpsplay/activeandroid/MainActivity.java
@@ -6,11 +6,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ExpandableListView;
 
-import java.util.ArrayList;
-
-import comstoresearchqmobotech.google.httpsplay.activeandroid.adapter.ExpandableListAdapter;
-import comstoresearchqmobotech.google.httpsplay.activeandroid.model.Child;
-import comstoresearchqmobotech.google.httpsplay.activeandroid.model.Group;
 import comstoresearchqmobotech.google.httpsplay.activeandroid.parsing.Apis;
 import comstoresearchqmobotech.google.httpsplay.activeandroid.parsing.MyParser;
 import comstoresearchqmobotech.google.httpsplay.activeandroid.toast.MyToast;
@@ -19,8 +14,6 @@ import comstoresearchqmobotech.google.httpsplay.activeandroid.utils.MyUtils;
 public class MainActivity extends AppCompatActivity {
 
     private ExpandableListView expandableListView;
-    private ExpandableListAdapter adapter;
-    private ArrayList<Group> expListItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,44 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getData();
         expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
-        expandableListView.setGroupIndicator(null);
-        expListItems = setStandardGroup();
-        expandableListView.setAdapter(new ExpandableListAdapter(this , expListItems));
 
-    }
-
-    private ArrayList<Group> setStandardGroup() {
-
-        int group_image_ring[] = {R.drawable.ring , R.mipmap.ic_launcher , R.mipmap.ic_launcher};
-        String group_num[] = {"1" , "2" , "3"};
-        String group_title[] = {"title1" , "title2" , "title3"};
-        String group_description[] = {"Here is mah desc1 honey" , "Here is mah desc2 honey" , "Here is mah desc3 honey"};
-        int child_image_share[] = {R.drawable.ring , R.mipmap.ic_launcher , R.drawable.ring};
-
-        ArrayList<Group> list = new ArrayList<>();
-        ArrayList<Child> list_child;
-        int size = 1 ;
-        int j = 0;
-
-        for(int i = 0 ; i < group_title.length ; i++){
-            Group group = new Group();
-            group.setGroupTitle(group_title[i]);
-            group.setGroupNum(group_num[i]);
-            group.setGroupDescription(group_description[i]);
-            group.setGroupImageRing(group_image_ring[i]);
-
-            list_child = new ArrayList<>();
-            for(; j < size ; j++){
-
-                Child child = new Child();
-                child.setChildImage(child_image_share[j]);
-                list_child.add(child);
-            }
-            group.setItem(list_child);
-            list.add(group);
-            size = size+1;
-        }
-        return list;
     }
 
 
--- app/src/main/java/comstoresearchqmobotech/google/httpsplay/activeandroid/model/Group.java
+++ app/src/main/java/comstoresearchqmobotech/google/httpsplay/activeandroid/model/Group.java
@@ -1,56 +1,9 @@
 package comstoresearchqmobotech.google.httpsplay.activeandroid.model;
 
-import java.util.ArrayList;
-
 /**
  * Created by Dell on 11/19/2015.
  */
 public class Group {
 
-    private Integer groupImageRing;
-    private String groupNum;
-    private String groupTitle;
-    private String groupDescription;
-
-    private ArrayList<Child> item;
-
-    public Integer getGroupImageRing() {
-        return groupImageRing;
-    }
-
-    public void setGroupImageRing(Integer groupImageRing) {
-        this.groupImageRing = groupImageRing;
-    }
-
-    public String getGroupNum() {
-        return groupNum;
-    }
-
-    public void setGroupNum(String groupNum) {
-        this.groupNum = groupNum;
-    }
-
-    public String getGroupTitle() {
-        return groupTitle;
-    }
-
-    public void setGroupTitle(String groupTitle) {
-        this.groupTitle = groupTitle;
-    }
-
-    public String getGroupDescription() {
-        return groupDescription;
-    }
-
-    public void setGroupDescription(String groupDescription) {
-        this.groupDescription = groupDescription;
-    }
-
-    public ArrayList<Child> getItem() {
-        return item;
-    }
-
-    public void setItem(ArrayList<Child> item) {
-        this.item = item;
-    }
+  
 }
